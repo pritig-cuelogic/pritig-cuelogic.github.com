d33176c5cf0781b3ccc94aefb25f39df673a6b74
pritig-cuelogic <priti.gupta@cuelogic.co.in>
2016-11-16 13:24:58 +0530

    Delete JavaAppletExample.java

--- JavaAppletExample.java
+++
@@ -1,8 +0,0 @@
-import java.applet.Applet;
-import java.awt.Graphics;
-
-public class JavaAppletExample extends Applet {
-  public void paint(Graphics g) {
-    g.drawString("Hello World!", 200, 100);
-  }
-}
