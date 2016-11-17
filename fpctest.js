caca36370b4c00d0aa3ae2a36e59ef726c3e5307
tusharncuelogic <tushar.nikam@cuelogic.co.in>
2016-11-17 13:09:25 +0530

    Adding the fucntion to the cookie name

--- fpctest.js
+++ fpctest.js
@@ -75,6 +75,29 @@ getDeviceInfo();
 var fingerprint = getFingerPrint(); 
 console.log(fingerprint);
 
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+
+
 function getScreenInfo () {
     var colorDepth = screen.colorDepth;
     var screenWidth = screen.width;
