028dd4aef3904429f0b7c29cd5d94266fd9a7551
Priti Gupta <priti.gupta@cuelogic.co.in>
2016-11-17 14:42:17 +0530

    Changes

--- fpctest.js
+++ fpctest.js
@@ -1,126 +1,86 @@
 'use strict';
 
-function getLocalIP (callback) {
-    var ip_dups = {};
-    var RTCPeerConnection = window.RTCPeerConnection
-        || window.mozRTCPeerConnection
-        || window.webkitRTCPeerConnection;
-    var useWebKit = !!window.webkitRTCPeerConnection;
-
-    var mediaConstraints = {
-        optional: [{RtpDataChannels: true}]
-    };
-
-    var servers = {iceServers: []};
-
-    var pc = new RTCPeerConnection(servers, mediaConstraints);
-
-    function handleCandidate (candidate) {
-        var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-        var ip_addr = ip_regex.exec(candidate)[1];
-
-        if(ip_dups[ip_addr] === undefined)
-            callback(ip_addr);
-
-        ip_dups[ip_addr] = true;
-    }
-
-    pc.onicecandidate = function (ice) {
-
-        if(ice.candidate)
-            handleCandidate(ice.candidate.candidate);
-    };
-
-    pc.createDataChannel("");
-
-    pc.createOffer(function(result){
-
-        pc.setLocalDescription(result, function(){}, function(){});
-
-    }, function(){});
-
-}
-var client = new ClientJS();
-
-function getFingerPrint () {
-    
-    
-    var canvasPrint = client.getCanvasPrint();
-    
-    var fingerprint = client.getCustomFingerprint(canvasPrint);
-    
-    return fingerprint;
-}
-
-
-function getDeviceInfo () {
-    
-    var OS = client.getOS();
-    var isMobile = client.isMobile();
-    var device;
-    if(isMobile) {
-        device = 'Mobile';
-    }
-    else {
-        device = 'Desktop';
-    }
-    console.log(device);
-    console.log(OS);
-}
-
-var browser = client.getBrowser();
-console.log(browser);
-
-getDeviceInfo();
-var fingerprint = getFingerPrint(); 
-console.log(fingerprint);
-
-function getCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
-    }
-    else
-    {
-        begin += 2;
-        var end = document.cookie.indexOf(";", begin);
-        if (end == -1) {
-        end = dc.length;
-        }
-    }
-    // because unescape has been deprecated, replaced with decodeURI
-    //return unescape(dc.substring(begin + prefix.length, end));
-    return decodeURI(dc.substring(begin + prefix.length, end));
-} 
-
-
-
-function getScreenInfo () {
-    var colorDepth = screen.colorDepth;
-    var screenWidth = screen.width;
-    var screenHeight = screen.height;
-    console.log(colorDepth);
-    console.log(screenWidth);
-    console.log(screenHeight);
-}
-
-getScreenInfo();
-getLocalIP(
-    function(ip) {
-        console.log(ip);
-    });
-
-jQuery.ajax( {
-    url: 'http://172.21.32.18:3000',
-    type: 'GET',
-    async: false,
-    success: function( response ) {
-        console.log(response);
+var fpc = {
+    init: function () {
+        var client = new ClientJS();
+        var fingerprint = fpc.getFingerPrint(client);
+        var deviceInfo = fpc.getDeviceInfo(client);
+        var screenInfo = fpc.getScreenInfo();
+        var browserName = fpc.getBrowser(client);
+        var localIp = fpc.getLocalIP(
+            function(ip) {
+                console.log(ip);
+                
+            });
+        
+        },
+    getFingerPrint: function (client) {
+        var canvasPrint = client.getCanvasPrint();
+        var fingerprint = client.getCustomFingerprint(canvasPrint);
+        return fingerprint;
     },
-    error: function(jqxhr) {
-           window.location.href = "/404";
-          }
-} );
+    getDeviceInfo: function (client) {
+        var os = client.getOS();
+        var isMobile = client.isMobile();
+        var device;
+        if(isMobile) {
+        device = 'Mobile';
+        }
+        else {
+        device = 'Desktop';
+        }
+        var deviceInfo = {
+           os: os,
+           device: device 
+        };
+        return deviceInfo;
+    },
+    getScreenInfo: function () {
+        var colorDepth = screen.colorDepth;
+        var screenWidth = screen.width;
+        var screenHeight = screen.height;
+        var screenInfo = {
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
+            colorDepth: colorDepth 
+    };
+        return screenInfo;
+    },
+    getLocalIP: function (callback) {
+        var ipDuplicate = {};
+        var RTCPeerConnection = window.RTCPeerConnection
+            || window.mozRTCPeerConnection
+            || window.webkitRTCPeerConnection;
+        var useWebKit = !window.webkitRTCPeerConnection;
+        var mediaConstraints = {
+            optional: [{RtpDataChannels: true}]
+        };
+        var servers = {iceServers: []};
+        var pc = new RTCPeerConnection(servers, mediaConstraints);
+
+        function handleCandidate (candidate) {
+            var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+            var ip_addr = ip_regex.exec(candidate)[1];
+            if(ipDuplicate[ip_addr] === undefined) {
+                callback(ip_addr);
+            }
+            ipDuplicate[ip_addr] = true;
+        }
+        pc.onicecandidate = function (ice) {
+            if(ice.candidate) {
+                handleCandidate(ice.candidate.candidate);
+            }
+        };
+        pc.createDataChannel("");
+        pc.createOffer(function(result){
+            pc.setLocalDescription(result, function(){}, function(){});
+        }, function(){});
+
+    },
+    getBrowser: function (client) {
+        var browser = client.getBrowser();
+        return browser;
+    }
+};
+
+fpc.init();
